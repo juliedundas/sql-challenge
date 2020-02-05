99142c06762304025e25be3fcb71b67ba3715d0b
juliedundas <juliedundas@Julies-MacBook-Pro-2.local>
2020-02-04 23:21:38 -0700

    updated erd

---
+++ Resources/QuickDBD-Pewlett Hackard ERD.sql
@@ -0,0 +1,80 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/FsBvac
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Departments" (
+    "dept_no" INTEGER   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "gender" VARCHAR(30)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Titles" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_name");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
--- Resources/schema.sql
+++ Resources/schema.sql
@@ -1,89 +1,66 @@
--- Create tables and import data
--- Drop table if exists
+--Clear tables if they already exist
 DROP TABLE IF EXISTS departments;
-
--- Create new table
-CREATE TABLE departments (
-  dept_no VARCHAR,
-  dept_name VARCHAR
-);
-
--- Verify successful data import
-SELECT * FROM departments;
-
--- Create tables and import data
--- Drop table if exists
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS dept_emp;
-
--- Create new table
-CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR,
-	from_date DATE,
-	to_date DATE
-);
-
--- Verify successful data import
-SELECT * FROM dept_emp;
-
--- Create tables and import data
--- Drop table if exists
 DROP TABLE IF EXISTS dept_manager;
 
--- Create new table
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no INT,
-	from_date DATE,
-	to_date DATE
+
+--Create tables for the 6 CSV files. Notate the primary and foreign keys when applicable
+CREATE TABLE departments (
+	dept_no VARCHAR(10) PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
 );
 
--- Verify successful data import
-SELECT * FROM dept_manager;
-
--- Create tables and import data
--- Drop table if exists
-DROP TABLE IF EXISTS employees;
-
--- Create new table
 CREATE TABLE employees (
-	emp_no INT,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	gender VARCHAR,
-	hire_date DATE
+	emp_no INTEGER PRIMARY KEY,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR(5) NOT NULL,
+	hire_date DATE NOT NULL
 );
 
--- Verify successful data import
-SELECT * FROM employees;
-
--- Create tables and import data
--- Drop table if exists
-DROP TABLE IF EXISTS salaries;
-
--- Create new table
 CREATE TABLE salaries (
-	emp_no INT,
-	salary INT,
-	from_date DATE,
-	to_date DATE
+	emp_no INTEGER PRIMARY KEY,
+	salary INTEGER NOT NULL,
+	from_date INTEGER NOT NULL,
+	to_date INTEGER NOT NULL
 );
 
--- Verify successful data import
-SELECT * FROM salaries;
-
--- Create tables and import data
--- Drop table if exists
-DROP TABLE IF EXISTS titles;
-
--- Create new table
 CREATE TABLE titles (
-	emp_no INT,
-	title VARCHAR,
-	from_date DATE,
-	to_date DATE
+	emp_no INTEGER NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY(emp_no, title, from_date)
 );
 
--- Verify successful data import
-SELECT * FROM titles;
+CREATE TABLE dept_emp(
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no, from_date)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (dept_no, emp_no, from_date)
+);
+
+--Verify successful data import
+SELECT * from departments;
+SELECT * from employees;
+SELECT * from titles;
+SELECT * from salaries;
+SELECT * from dept_emp;
+SELECT * from dept_manager;
